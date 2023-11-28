791b19a45ef812996eab823935e4f8b30fa6d3b1
Janiheryson Felipe <106495257+janiherysonFelipe@users.noreply.github.com>
2023-11-28 19:35:40 -0300

    Delete AtualizarAlunoDTO.java

--- AtualizarAlunoDTO.java
+++
@@ -1,6 +0,0 @@
-package br.ufrn.imd.universidade.model;
-
-import java.util.Date;
-public record AtualizarAlunoDTO( Curso curso, String nomeAluno,  String cpf, String telefone, Date nascimento) {
-
-}
